d64793dca35c00ee71117e2b1eb9a992b910a3ea
Matu Aravena <matias.aravena.g@gmail.com>
2024-09-12 21:15:16 +0200

    Improving code

--- vptest.client/src/App.tsx
+++ vptest.client/src/App.tsx
@@ -9,13 +9,25 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
 function App() {
-    const [listTasks, setListTasks] = useState<TaskModel[]>();
+    const [listTasks, setListTasks] = useState<TaskModel[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
 
     const fetchTasks = async () => {
-        const tasks = await getTasks();
 
-        if (tasks && tasks.data.length > 0) {
-            setListTasks(tasks.data);
+        try {
+            const tasks = await getTasks();
+
+            if (tasks?.data?.length > 0)
+                setListTasks(tasks.data);
+            else
+                setError('No tasks found')
+
+        } catch (e) {
+            setError('Failed to fetch tasks :' + e);
+        }
+        finally {
+            setLoading(false);
         }
     };
 
@@ -25,55 +37,77 @@ function App() {
 
     //Actions
     const handleAddTask = async (newTask: TaskModel): Promise<Boolean> => {
+        try {
+            const result = await addTask(newTask);
 
-        const result = await addTask(newTask);
+            if ((result as ErrorType)?.error) {
+                console.log('handleAddTask result', result)
+                return false;
+            }
 
-        if ((result as ErrorType)?.error) {
-            console.log('handleAddTask result', result)
+            setListTasks(prevState => [...prevState as TaskModel[], newTask]);
+            return true;
+
+        } catch (err) {
+            console.error('Error adding task:', err);
+            setError('Failed to add task');
             return false;
         }
-
-        setListTasks(prevState => [...prevState as TaskModel[], newTask]);
-        return true;
     };
 
-    const handleDelete = async (id: number) => {
-        const result = await deleteTask(id);
+    const handleDeleteTasks = async (id: number) => {
+        try {
+            const result = await deleteTask(id);
 
-        if ((result as ErrorType)?.error) {
-            console.log('handleDelete  result', result)
-            return;
+            if ((result as ErrorType)?.error) {
+                console.log('handleDelete  result :', result)
+                return;
+            }
+
+            setListTasks(listTasks?.filter(task => task.id !== id));
+        } catch (err) {
+            console.error('Error deleting task:', err);
+            setError('Failed to delete task');
         }
-        setListTasks(listTasks?.filter(task => task.id !== id));
     };
 
-    const handleUpdate = async (task: TaskModel) => {
-        const result = await updateTask(task);
+    const handleUpdateTasks = async (task: TaskModel) => {
+        try {
+            const result = await updateTask(task);
 
-        if ((result as ErrorType)?.error) {
-            console.log('handleUpdate result', result)
-            return;
+            if ((result as ErrorType)?.error) {
+                console.log('handleUpdate result :', result)
+                return;
+            }
+
+            //map improve logic and avoid possible re-render issues
+            setListTasks(prevTasks => prevTasks.map(t => (t.id === task.id ? task : t)));
+
+        } catch (err) {
+            console.error('Error updating task:', err);
+            setError('Failed to update task');
         }
-
-        setListTasks(prevTasks => {
-            return [...prevTasks?.filter(t => t.id !== task.id) ?? [], task];
-        });
     };
 
     //Components to display
-    const formComponent = listTasks ?
+    const formComponent = !loading ?
         <TaskForm onAddTask={handleAddTask} />
-        : <p><em>Loading... Please wait a few seconds o refresh the page.</em></p>
+        : <p><em>Loading... Please wait or refresh the page.</em></p>;
 
-    const listComponent = listTasks && listTasks.length > 0 ?
-        <TaskList tasks={listTasks} onDeleteTask={handleDelete} onUpdateTask={handleUpdate} />
-        : <p><em>Probably Back end is loading.</em></p>
+    const listComponent = loading ?
+        <p><em>Loading tasks...</em></p>
+        : listTasks.length > 0 ?
+            <TaskList tasks={listTasks} onDeleteTask={handleDeleteTasks} onUpdateTask={handleUpdateTasks} />
+            : <p><em>No tasks available.</em></p>;
+            
+    const displayError = error ? <p><br />{error}<br /></p> : <></>
 
     //Return HTML
     return (
         <div >
             <LocalizationProvider dateAdapter={AdapterDayjs}>
                 <h1 id="tabelLabel">List of ToDos</h1>
+                {displayError}
                 {formComponent}
                 <br />
                 {listComponent}
