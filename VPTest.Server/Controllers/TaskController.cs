406867cbc7a549008cf85ad211253e9cd98a7e79
Matu Aravena <matias.aravena.g@gmail.com>
2024-09-12 22:07:24 +0200

    under the concept SoC "Separation of Concerns" i have separate all the possible elements from the controller

--- VPTest.Server/Controllers/TaskController.cs
+++ VPTest.Server/Controllers/TaskController.cs
@@ -2,57 +2,41 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 using VPTest.Server.Data;
+using VPTest.Server.Interfaces;
 
 namespace VPTest.Server.Controllers
 {
-    public interface ITaskRepository
+    public class TaskRepository : ITaskRepository
     {
-        Task<IEnumerable<TaskModel>> GetTasks();
-        Task<TaskModel> AddTask(TaskModel task);
-        Task<bool> UpdateTask(TaskModel task);
-        Task<bool> DeleteTask(int id);
-    }
-
-    [ApiController]
-    [Route("api/[controller]")]
-    public class TaskController : ControllerBase, ITaskRepository
-    {
-        private TasksDbContext _context;
-        public TaskController(TasksDbContext context)
+        private readonly TasksDbContext _context;
+        public TaskRepository(TasksDbContext context)
         {
             _context = context;
         }
 
-        [HttpGet(Name = "GetTasks")]
-        public async Task<IEnumerable<TaskModel>> GetTasks()
-        {
-            return await _context.TasksModels.ToListAsync();
-        }
+        public async Task<IEnumerable<TaskModel>> GetTasks() => await _context.TasksModels.ToListAsync();
 
-        [HttpPost(Name = "AddTask")]
-        public async Task<TaskModel> AddTask(TaskModel _task)
+        public async Task<TaskModel> AddTask(TaskModel task)
         {
-            _task.Id = _context.TasksModels.Select(x => x.Id).Max() + 1;
-            _context.TasksModels.Add(_task);
+            task.Id = _context.TasksModels.Select(x => x.Id).Max() + 1;
+            _context.TasksModels.Add(task);
             await _context.SaveChangesAsync();
-            return _task;
+            return task;
         }
 
-        [HttpPut(Name = "UpdateTask")]
-        public async Task<bool> UpdateTask(TaskModel _task)
+        public async Task<bool> UpdateTask(TaskModel task)
         {
-            var existingTask = await _context.TasksModels.FindAsync(_task.Id);
+            var existingTask = await _context.TasksModels.FindAsync(task.Id);
             if (existingTask == null) return false;
 
-            existingTask.Description = _task.Description;
-            existingTask.Deadline = _task.Deadline;
-            existingTask.IsDone = _task.IsDone;
+            existingTask.Description = task.Description;
+            existingTask.Deadline = task.Deadline;
+            existingTask.IsDone = task.IsDone;
 
             await _context.SaveChangesAsync();
             return true;
         }
 
-        [HttpDelete(Name = "DeleteTask")]
         public async Task<bool> DeleteTask(int id)
         {
             var task = await _context.TasksModels.FindAsync(id);
@@ -61,6 +45,42 @@ namespace VPTest.Server.Controllers
             _context.TasksModels.Remove(task);
             await _context.SaveChangesAsync();
             return true;
+        }
+    }
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TaskController : ControllerBase
+    {
+        private readonly ITaskRepository _taskRepository;
+
+        public TaskController(ITaskRepository taskRepository)
+        {
+            _taskRepository = taskRepository;
+        }
+
+        [HttpGet(Name = "GetTasks")]
+        public async Task<IEnumerable<TaskModel>> GetTasks()
+        {
+            return await _taskRepository.GetTasks();
+        }
+
+        [HttpPost(Name = "AddTask")]
+        public async Task<TaskModel> AddTask(TaskModel task)
+        {
+            return await _taskRepository.AddTask(task);
+        }
+
+        [HttpPut(Name = "UpdateTask")]
+        public async Task<bool> UpdateTask(TaskModel task)
+        {
+            return await _taskRepository.UpdateTask(task);
+        }
+
+        [HttpDelete(Name = "DeleteTask")]
+        public async Task<bool> DeleteTask(int id)
+        {
+            return await _taskRepository.DeleteTask(id);
         }
     }
 }
---
+++ VPTest.Server/Interfaces/ITaskRepository.cs
@@ -0,0 +1,12 @@
+﻿using VPTest.Server.Data;
+
+namespace VPTest.Server.Interfaces
+{
+    public interface ITaskRepository
+    {
+        Task<IEnumerable<TaskModel>> GetTasks();
+        Task<TaskModel> AddTask(TaskModel task);
+        Task<bool> UpdateTask(TaskModel task);
+        Task<bool> DeleteTask(int id);
+    }
+}
