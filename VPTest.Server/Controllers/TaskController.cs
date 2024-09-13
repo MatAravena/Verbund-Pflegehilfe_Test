45acac68cb76ea51a26f73947d468e7519bcd0f2
Matu Aravena <matias.aravena.g@gmail.com>
2024-09-13 17:20:06 +0200

    created a implementation class to separate the functionality from the class controller And Controller is properly managing Http messages

--- VPTest.Server/Controllers/TaskController.cs
+++ VPTest.Server/Controllers/TaskController.cs
@@ -1,53 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using System.Threading.Tasks;
-using VPTest.Server.Data;
 using VPTest.Server.Interfaces;
 
 namespace VPTest.Server.Controllers
 {
-    public class TaskRepository : ITaskRepository
-    {
-        private readonly TasksDbContext _context;
-        public TaskRepository(TasksDbContext context)
-        {
-            _context = context;
-        }
-
-        public async Task<IEnumerable<TaskModel>> GetTasks() => await _context.TasksModels.ToListAsync();
-
-        public async Task<TaskModel> AddTask(TaskModel task)
-        {
-            task.Id = _context.TasksModels.Select(x => x.Id).Max() + 1;
-            _context.TasksModels.Add(task);
-            await _context.SaveChangesAsync();
-            return task;
-        }
-
-        public async Task<bool> UpdateTask(TaskModel task)
-        {
-            var existingTask = await _context.TasksModels.FindAsync(task.Id);
-            if (existingTask == null) return false;
-
-            existingTask.Description = task.Description;
-            existingTask.Deadline = task.Deadline;
-            existingTask.IsDone = task.IsDone;
-
-            await _context.SaveChangesAsync();
-            return true;
-        }
-
-        public async Task<bool> DeleteTask(int id)
-        {
-            var task = await _context.TasksModels.FindAsync(id);
-            if (task == null) return false;
-
-            _context.TasksModels.Remove(task);
-            await _context.SaveChangesAsync();
-            return true;
-        }
-    }
-
     [ApiController]
     [Route("api/[controller]")]
     public class TaskController : ControllerBase
@@ -60,27 +15,37 @@ namespace VPTest.Server.Controllers
         }
 
         [HttpGet(Name = "GetTasks")]
-        public async Task<IEnumerable<TaskModel>> GetTasks()
+        public async Task<IActionResult> GetTasks()
         {
-            return await _taskRepository.GetTasks();
+            var tasks = await _taskRepository.GetAllTasksAsync();
+            return Ok(tasks);
         }
 
         [HttpPost(Name = "AddTask")]
-        public async Task<TaskModel> AddTask(TaskModel task)
+        public async Task<IActionResult> AddTask(TaskDTO task)
         {
-            return await _taskRepository.AddTask(task);
+            var newTasl = await _taskRepository.CreateTaskAsync(task);
+            return CreatedAtAction(nameof(GetTasks), new { id = newTasl.Id }, newTasl);
         }
 
         [HttpPut(Name = "UpdateTask")]
-        public async Task<bool> UpdateTask(TaskModel task)
+        public async Task<IActionResult> UpdateTask(TaskDTO task)
         {
-            return await _taskRepository.UpdateTask(task);
+            var success = await _taskRepository.UpdateTaskAsync(task);
+            if (!success)
+                return NotFound(new { Message = "Task not found." });
+
+            return NoContent();
         }
 
         [HttpDelete(Name = "DeleteTask")]
-        public async Task<bool> DeleteTask(int id)
+        public async Task<IActionResult> DeleteTask(int id)
         {
-            return await _taskRepository.DeleteTask(id);
+            var success = await _taskRepository.DeleteTaskAsync(id);
+            if (!success)
+                return NotFound(new { Message = "Task not found." });
+
+            return NoContent();
         }
     }
 }
---
+++ VPTest.Server/Implementation/TaskRepository.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VPTest.Server.Data;
+using VPTest.Server.Interfaces;
+using VPTest.Server.Model;
+
+namespace VPTest.Server.Implementation
+{
+    public class TaskRepository: ITaskRepository
+    {
+        private readonly TasksDbContext _context;
+
+        public TaskRepository(TasksDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<TaskDTO>> GetAllTasksAsync()
+        {
+            var tasks = await _context.TasksModels.ToListAsync();
+            return TaskDTO.MapToDTO(tasks);
+        }
+
+        public async Task<TaskDTO> CreateTaskAsync(TaskDTO task)
+        {
+            task.Id = _context.TasksModels.Any() ? _context.TasksModels.Max(x => x.Id) + 1 : 1;
+            var newTask = TaskModel.createNewTask(task.Id, task.Description, task.Deadline, task.IsDone);
+
+            _context.TasksModels.Add(newTask);
+            await _context.SaveChangesAsync();
+
+            return task;
+        }
+
+        public async Task<bool> UpdateTaskAsync(TaskDTO task)
+        {
+            var existingTask = await _context.TasksModels.FindAsync(task.Id);
+            if (existingTask == null) return false;
+
+            existingTask.Description = task.Description;
+            existingTask.Deadline = task.Deadline;
+            existingTask.IsDone = task.IsDone;
+
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> DeleteTaskAsync(int id)
+        {
+            var task = await _context.TasksModels.FindAsync(id);
+            if (task == null) return false;
+
+            _context.TasksModels.Remove(task);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
