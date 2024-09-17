7ba64c7b31671e6facc0979881b4a30bcc9a3a46
Matu Aravena <matias.aravena.g@gmail.com>
2024-09-17 22:55:50 +0200

    fix mockApi

--- vptest.client/src/Tests/mockApi.ts
+++ vptest.client/src/Tests/mockApi.ts
@@ -1,24 +1,20 @@
 import { Page } from '@playwright/test';
 
 export async function mockApi(page: Page) {
-
-    await page.route('/api/tasks', async (route) => {
-        route.fulfill({
-            status: 200,
-            body: JSON.stringify([
-                { id: 1, description: 'Mock Task 1', deadline: '2024-01-01', isDone: false },
-                { id: 2, description: 'Mock Task 2', deadline: '2024-02-01', isDone: true },
-                { id: 3, description: 'Mock Task 3', deadline: '2024-03-01', isDone: false },
-                { id: 4, description: 'Mock Task 4', deadline: '2024-04-01', isDone: true },
-                { id: 5, description: 'Mock Task 5', deadline: '2024-05-01', isDone: false },
-            ]),
-        });
-    });
-
-
-    await page.route('/api/task', async (route) => {
-
+    await page.route('**/api/task', async (route) => {
         switch (route.request().method()) {
+            case 'GET':
+                route.fulfill({
+                    status: 200,
+                    body: JSON.stringify([
+                        { id: 1, description: 'Mock Task 1', deadline: '2024-01-01', isDone: false },
+                        { id: 2, description: 'Mock Task 2', deadline: '2024-02-01', isDone: true },
+                        { id: 3, description: 'Mock Task 3', deadline: '2024-03-01', isDone: false },
+                        { id: 4, description: 'Mock Task 4', deadline: '2024-04-01', isDone: true },
+                        { id: 5, description: 'Mock Task 5', deadline: '2024-05-01', isDone: false },
+                    ]),
+                });
+                break;
             case 'POST':
                 route.fulfill({
                     status: 201,
@@ -26,7 +22,7 @@ export async function mockApi(page: Page) {
                     headers: { 'Content-Type': 'application/json' }
                 });
                 break;
-            case 'PUT' :
+            case 'PUT':
             case 'DELETE':
                 route.fulfill({
                     status: 200,
